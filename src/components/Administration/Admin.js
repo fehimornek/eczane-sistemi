7a3174d71eece197ac9f95ee8301a0395a486242
fehim <fehimornek@gmail.com>
2022-05-26 19:18:50 +0300

    wrote a function to pick just a month from the calender

--- src/components/Administration/Admin.js
+++ src/components/Administration/Admin.js
@@ -6,12 +6,47 @@ import Login from "./Login"
 import "./Admin.css"
 
 const Admin = () => {
+    const minMaxDate = () => {
+        const fullDate = new Date()
+        let day = fullDate.getDay() 
+        let month = fullDate.getMonth() + 1
+        let year = fullDate.getFullYear()
+        let date = fullDate.getDate()
+        var lengthOfMonth= new Date(year, month+1, 0).getDate();
+         
+        let nextMonthPickableDays = 30 - (lengthOfMonth - date + 1)
+        let maxDate = lengthOfMonth
+        let maxYear = year
+        let maxMonth = month
+
+        // if it's not the beginning of the month (which is very unlikely)
+        if (nextMonthPickableDays > 0) {
+            maxDate = nextMonthPickableDays
+            maxMonth = month + 1
+            if (maxMonth === 13){
+                maxMonth = 1
+                maxYear = year + 1
+            }
+        }
+        if (date < 10) date = "0" + date
+
+        if (month < 10) month = "0" + month
+
+        if (maxMonth < 10) maxMonth = "0" + maxMonth 
+        
+        if (maxDate < 10) maxDate = "0" + maxDate
+    
+        const minimumDate = year + "-" + month + "-" + date
+        const maximumDate = maxYear + "-" + maxMonth + "-" + maxDate
+        return [minimumDate, maximumDate]
+    }
+    let dates = minMaxDate()
     return (
         <div className='grid_container'>
             <Login></Login>
-            <TekTek></TekTek>
-            <Toplu></Toplu>
-            <Otomatik></Otomatik>
+            <TekTek dates={dates}></TekTek>
+            <Toplu dates={dates}></Toplu>
+            <Otomatik dates={dates}></Otomatik>
         </div>
     )
 }
