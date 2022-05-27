97c2459397fba6b1fe8f2d90b9ed126e7f8ccbfb
fehim <fehimornek@gmail.com>
2022-05-27 15:34:54 +0300

    indexCalculator is now in misc file to be used by main page

--- src/components/MainPage/PickDate.js
+++ src/components/MainPage/PickDate.js
@@ -1,14 +1,29 @@
 import React from "react";
+import indexCalculator from "../misc/indexCalculator";
 
-const PickDate = ({dates}) => {
+const PickDate = ({dates, setShowDate}) => {
     let minDate = dates[0]
     let maxDate = dates[1]
+    let lenMonth = dates[2]
+
+    const eczaneGosterBugun = () => {
+        setShowDate(0)
+    }
+    const eczaneGosterYarin = () => {
+        setShowDate(1)
+    }
+    const eczaneGosterTarih = () =>{
+        let index = indexCalculator("tarih_main", minDate, lenMonth)
+        setShowDate(index)
+    }
+
     return (
         <div className="pickDate">
             <h2>Bir gün veya tarih seçin</h2>
-            <button>Bugün</button>
-            <button>Yarın</button>
-            <input type="date" min={minDate} max={maxDate}></input>
+            <button onClick={eczaneGosterBugun}>Bugün</button>
+            <button onClick={eczaneGosterYarin}>Yarın</button>
+            <input id="tarih_main" type="date" min={minDate} max={maxDate}></input>
+            <button onClick={eczaneGosterTarih}></button>
         </div>
     )
 }
