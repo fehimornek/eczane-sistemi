98f73a890c10c1b78addd4aa4d1a3ad1a4189873
fehim <fehimornek@gmail.com>
2022-05-27 15:17:09 +0300

    dates now cant exceed a month from the current date

--- src/components/MainPage/Main.js
+++ src/components/MainPage/Main.js
@@ -1,10 +1,17 @@
 import React from "react";
+import LoginNheader from "./LoginNheader";
+import PickDate from "./PickDate";
+import ShowEczane from "./ShowEczane";
+import "./Main.css"
+import minMaxDate from "../misc/minMaxDate";
 
 const Main = () => {
-
+    let dates = minMaxDate()
     return (
-        <div className="grid_container">
-
+        <div className="grid_container_main">
+            <LoginNheader></LoginNheader>
+            <PickDate dates={dates}></PickDate>
+            <ShowEczane></ShowEczane>
         </div>
     )
 }
--- src/components/MainPage/PickDate.js
+++ src/components/MainPage/PickDate.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-const PickDate = () => {
-
+const PickDate = ({dates}) => {
+    let minDate = dates[0]
+    let maxDate = dates[1]
     return (
-        <div >
-
+        <div className="pickDate">
+            <h2>Bir gün veya tarih seçin</h2>
+            <button>Bugün</button>
+            <button>Yarın</button>
+            <input type="date" min={minDate} max={maxDate}></input>
         </div>
     )
 }
